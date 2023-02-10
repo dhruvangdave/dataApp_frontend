829d0052a31cdbc7c878111b69d48f66d7aadded
dhruvangdave <dhruvang.dave@ghanshyamdigital.com>
2023-02-10 20:21:12 +0530

    Search component added

--- src/components/UseEff.js
+++ src/components/UseEff.js
@@ -2,16 +2,17 @@ import { React, useEffect, useState } from "react";
 
 const useEff = () => {
   const [drinkList, setDrinkList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    fetcher()
+  }, [searchQuery]);
 
   const fetcher = () => {
-    // setDrinkList([{
-    //     "okay": "oka",
-    //     "okay": "oka",
-    //     "okay": "oka"
-    // }])
-    // console.log(drinkList);
-
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchQuery}`)
       .then((res) => {
         return res.json();
       })
@@ -19,23 +20,21 @@ const useEff = () => {
         setDrinkList(response.drinks);
       });
   };
-  console.log(drinkList);
-
-  useEffect(() => {
-    fetcher();
-  }, []);
 
   return (
     <>
     <div className="container">
+    <form className="d-flex my-4">
+        <input className="form-control me-2 text-center" onChange={handleChange}  value={searchQuery} type="search" placeholder="Search" name="search" aria-label="Search" />
+      </form>
       <div className="row">
-        {drinkList.map((ele, index) => {
-            
+        {drinkList === null && <h1>Data not found</h1> }
+        {drinkList !== null && drinkList.map((ele, index) => {
           return (
             <div className="col-md-4" key={index}>
               <img src={ele.strDrinkThumb} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Drinks to cheers</h5>
+                <h5 className="card-title">{ele.strDrink}</h5>
                 <h5 className="card-title">{ele.strGlass}</h5>
                 <p className="card-text">
                   {ele.strInstructions}
